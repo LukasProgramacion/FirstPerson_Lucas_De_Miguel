fb09fe5126d0d2500504acc4be6a906da780d3ae
LukasProgramacion <lukas.arts315@gmail.com>
2024-10-21 17:20:05 +0200

    Avance juego primera persona (21/10/24)

--- Assets/Scripts/FirstPerson.cs
+++ Assets/Scripts/FirstPerson.cs
@@ -7,6 +7,8 @@ public class FirstPerson : MonoBehaviour
     [SerializeField] private float velocidadMovimiento;
 
     CharacterController controller;
+
+    private Camera cam;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +16,8 @@ public class FirstPerson : MonoBehaviour
 
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+
+        cam = Camera.main;
     }
 
     // Update is called once per frame
@@ -27,10 +31,10 @@ public class FirstPerson : MonoBehaviour
         Vector2 input = new Vector2(h, v).normalized;
 
         //Si existe input...
-        if (input.magnitude > 0) 
+        if (input.sqrMagnitude > 0) 
         {
             //Se calcula el ánguulo al que tengo que rotarme en función de los inputs y orientación de la camara.
-            float anguloRotacion = Mathf.Atan2(input.x, input.y) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
+            float anguloRotacion = Mathf.Atan2(input.x, input.y) * Mathf.Rad2Deg + cam.transform.eulerAngles.y;
             transform.eulerAngles = new Vector3(0, anguloRotacion, 0);
 
             Vector3 movimiento = Quaternion.Euler(0, anguloRotacion, 0) * Vector3.forward;
