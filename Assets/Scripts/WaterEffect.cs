62968dad2160c92f3decd8703016c80e0e829180
LukasProgramacion <lukas.arts315@gmail.com>
2024-11-25 18:20:58 +0100

    Avance juego primera (25/11/2024)

--- Assets/Scripts/WaterEffect.cs
+++ Assets/Scripts/WaterEffect.cs
@@ -6,20 +6,24 @@ using UnityEngine.Rendering.Universal;
 
 public class WaterEffect : MonoBehaviour
 {
+    [SerializeField] private float velocidad;
+
     private Volume efecto;
     // Start is called before the first frame update
     void Start()
     {
         efecto = GetComponent<Volume>();
-    }
+    } 
 
     // Update is called once per frame
     void Update()
     {
         if (efecto.profile.TryGet(out LensDistortion distortion))
         {
-            //distortion.xMultiplier
-            //distortion.yMultiplier
+            FloatParameter miVariable = new FloatParameter(1 + Mathf.Cos(velocidad * Time.time)/2);
+            FloatParameter miVariable2 = new FloatParameter(1 + Mathf.Sin(velocidad * Time.time)/2);
+            distortion.xMultiplier.SetValue(miVariable);
+            distortion.yMultiplier.SetValue(miVariable2);
         }
     }
 }
